dafaba546a1c8e3c1923157594f43049bfd187f4
sjwatts119 <samuel.watts67@gmail.com>
2024-04-04 11:17:54 +0100

    Set Base Routes with Auth Middleware

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+//by default, the user will be redirected to the login page.
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('auth.login');
+})->middleware('guest');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
+    //here we are enforcing the user to be authenticated, and their account to be verified through email
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
