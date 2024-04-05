39a8da82938dee91260ce5142a89be6ba71c44bd
sjwatts119 <samuel.watts67@gmail.com>
2024-04-05 16:59:49 +0100

    If temperature data is not live, a warning will be displayed until live data arrives.

--- app/Livewire/Temperatures.php
+++ app/Livewire/Temperatures.php
@@ -15,9 +15,13 @@ class Temperatures extends Component
         if (!isset($this->currentInside)) {
             //initialize the currentInside value in the live view. This is necessary to populate the first value, before waiting for the first update.
             $this->currentInside = Temperature::latest()->first()->sensorInside;
+
+            //because we are pulling old data, we need to inform the user this data is stale and the live data is loading.
+            $this->insideStale = true;
         }
         if(!isset($this->currentOutside)) {
             $this->currentOutside = Temperature::latest()->first()->sensorOutside;
+            $this->outsideStale = true;
         }
     }
 
@@ -26,6 +30,10 @@ class Temperatures extends Component
         //update the currentInside value in the live view based on the new data from the pusher event, this method is called from the frontend.
         $this->currentInside = $newInside;
         $this->currentOutside = $newOutside;
+
+        //set the stale values to false, because we have new data.
+        $this->insideStale = false;
+        $this->outsideStale = false;
     }
 
     //the render function is called every time the component is updated, to render the view.
@@ -34,7 +42,9 @@ class Temperatures extends Component
         //return temperature view with currentInside and currentOutside values.
         return view('livewire.temperatures')->with([
             'currentInside' => $this->currentInside,
-            'currentOutside' => $this->currentOutside
+            'currentOutside' => $this->currentOutside,
+            'insideStale' => $this->insideStale,
+            'outsideStale' => $this->outsideStale
         ]);
     }
 }
--- resources/views/livewire/temperatures.blade.php
+++ resources/views/livewire/temperatures.blade.php
@@ -1,12 +1,17 @@
 <div class="flex flex-row w-full justify-center h-60">
     <x-card header="Inside Temperature">
         <x-slot name="body">
+            {{-- if the inside temperature is stale data, we need a small yellow piece of text to indicate this, it should be formatted right --}}
             {{-- show the inside temperature with a fa thermometer-half icon --}}
             <div class="flex flex-col items-center text-4xl">
+
                 <i class="fas fa-thermometer-half text-gray-500"></i>
                 {{--output the most recent temperature reading from the sensor. this is being passed in as $currentInside --}}
                 <p class="text-gray-500">{{$currentInside}}°C</p>
             </div>
+            @if ($insideStale)
+                <p class="text-yellow-500 text-xs text-right">Old Data!</p>
+            @endif
         </x-slot>
     </x-card>
 
@@ -18,6 +23,9 @@
                 {{--output the most recent temperature reading from the sensor. this is being passed in as $currentInside --}}
                 <p class="text-gray-500">{{$currentOutside}}°C</p>
             </div>
+            @if ($outsideStale)
+                <p class="text-yellow-500 text-xs text-right">Old Data!</p>
+            @endif
         </x-slot>
     </x-card>
 </div>
