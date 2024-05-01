d3c8b170a3ff9ab6f0a806e80fd6da7cfe364f1e
sjwatts119 <samuel.watts67@gmail.com>
2024-05-01 10:12:46 +0100

    Fixed Historical Data not Showing
    Because we were only taking 100 records, it seemed to be ignoring anything with an ID higher than 100.
    
    This solution isn't great. As the table expands, it will have to process every single record, which could result in thousands or more operations per table, but I don't have enough time to make a cleaner solution, so this will have to do for now.

--- app/Livewire/Heater.php
+++ app/Livewire/Heater.php
@@ -16,7 +16,7 @@ class Heater extends Component
 
     public function getUpdatedHistoricalData(){
         //get the last 50 records from the HeaterStatus model. This will be temporarily stored and used in the next step.
-        $allHeaterRecords = HeaterRecord::orderBy('created_at', 'asc')->take(50)->get();
+        $allHeaterRecords = HeaterRecord::orderBy('created_at', 'asc')->get();
 
         //we need to make a multidimensional array that will store each instance of where the heater was on.
         //we need to analyse the data in the $allHeaterRecords array and using the times where the status was true, and the next time it was false, we can calculate the time the heater was on.
@@ -50,6 +50,10 @@ class Heater extends Component
         }
         //reverse the array so the most recent records are at the top.
         $heaterRecords = array_reverse($heaterRecords);
+
+        //slice the records to only show the last 25 records maximum.
+        $heaterRecords = array_slice($heaterRecords, 0, 25);
+
 
         return $heaterRecords;
     }
